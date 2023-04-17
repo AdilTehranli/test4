8c6332b4b3404d5554a430bf7b79d39e73adbce4
AdilTehranli <adiljt@code.edu.az>
2023-04-17 18:53:49 +0400

    homework added

---
+++ homework/homework/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.CompilerServices;
+
+Console.WriteLine("aylari qeyd edin");
+string ay  = Console.ReadLine();
+switch (ay)
+{
+    case "Yanvar":
+    case "Fevral":
+        case "Dekabr":
+        Console.WriteLine("qis");
+        break;
+    case "mart":
+    case "aprel":
+    case "may":
+        Console.WriteLine("Yaz");
+        break;
+    case "iyun":
+    case "iyul":
+    case "avgust":
+        Console.WriteLine("yay");
+        break;
+    case "sentyabr":
+    case "oktyabr":
+    case "noyabr":
+        Console.WriteLine("payiz");
+        break;
+
+
+    default:
+        Console.WriteLine("duzgun fesil yazin");
+        break;
+        
+
+}
